f2b5301a10dd3863e7568627ce58024f3ca85938
Kaleb Zufanetti Santos <kalebzsantosk@gmail.com>
2022-12-27 12:36:44 -0300

    Update main.js

--- mochila-de-viagem/main.js
+++ mochila-de-viagem/main.js
@@ -1,23 +1,47 @@
-const form = document.getElementById("novoItem")
+// Operador lógico que retorna com dados salvos, ou string vazia, utilizando localStorage.getItem, modificando o valor de `string` com JSON.parse()
+
+const form = document.getElementById("novoItem") 
 const lista = document.getElementById("lista")
+const itens = JSON.parse(localStorage.getItem("itens")) || []   
 
-document.getElementById("novoItem")
+// Uso do forEach para que todos os itens já escritos na lista sejam mantidos ao atualizar a página 
+itens.forEach( (elemento) => {    
+    criaElemento(elemento)
+} )     
 
-Form.addEventListener("submit", (evento)=> {
-    evento.preventDefault()
+// Refatoração do addEventListener para receber as funções extras da função criaElemento
+form.addEventListener("submit", (evento) => {   
+    evento.preventDefault()            
 
-    criaElemento(evento.target.elements['nome'].value, evento.target.elements['quantidade'].value)
+    const nome = evento.target.elements['nome']
+    const quantidade = evento.target.elements['quantidade']
+
+    const itemAtual = {
+    "nome": nome.value,
+    "quantidade": quantidade.value
+    }
+
+    criaElemento(itemAtual)
+
+    itens.push(itemAtual)
+
+    localStorage.setItem("itens", JSON.stringify(itens))
+
+    nome.value = ""
+    quantidade.value = ""
 })
 
-function criaElemento(nome, quantidade) {
+// Refatoração da função `criaElemento` para que possua apenas a função que faça sentido ao nome. 
+
+function criaElemento(item) {  
     const novoItem = document.createElement('li')
     novoItem.classList.add("item")
 
     const numeroItem = document.createElement('strong')
-    numeroItem.innerHTML = quantidade
-
+    numeroItem.innerHTML = item.quantidade
     novoItem.appendChild(numeroItem)
-    novoItem.innerHTML += nome
-    
+
+    novoItem.innerHTML += item.nome
+
     lista.appendChild(novoItem)
 }
